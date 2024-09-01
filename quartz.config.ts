192a816e8bb89894570b4ab84e0072a644c741f5
Arty <arnavarr@gmail.com>
2024-09-01 12:00:03 +0200

    prueba

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Notas Arturo Navarro",
+    pageTitle: "Notas Arturo Navarro",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
